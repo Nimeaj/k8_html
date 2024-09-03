835cb421bfed5c305f7822209f98faf8cdb87cf2
Nimeaj <qwp0814@gmail.com>
2024-09-03 16:44:16 +0900

    HTML

---
+++ 03/03.js
@@ -0,0 +1,9 @@
+//function함수로
+function handleClick() {
+  alert("외부스크립트 : 안녕하세요.");
+}
+
+//ES6 : 화살표 함수
+const handleClick2 = () => {
+  alert("화살표 함수");
+}
---
+++ 06/06.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+  //이미지
+  const imgs = document.querySelectorAll('.dice > img');
+  //버튼
+  const bts = document.querySelectorAll('.bt > button');
+  //결과
+  const msg = document.querySelector('#msg');
+
+  //버튼클릭
+  for(let bt of bts) {
+    // console.log(bt);
+    bt.addEventListener('click', ()=>{
+      //컴퓨터 랜덤수
+      let comN = Math.floor(Math.random()*6) + 1 ;
+      imgs[0].setAttribute('src', `../img/${comN}.png`);
+      imgs[0].setAttribute('alt', `${comN}.png`);
+
+      //사용자 선택수
+      console.log(bt.textContent.charAt(0));
+      let userN = parseInt(bt.textContent.charAt(0));
+      imgs[1].setAttribute('src', `../img/${userN}.png`);
+      imgs[1].setAttribute('alt', `${comN}.png`);
+
+      //결과비교출력
+      if(comN === userN) {
+        msg.textContent = '맞음';
+      } else {
+        msg.textContent = '틀림';
+      }
+
+    });
+  }
+
+});
